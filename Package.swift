0e9cdf49460f93f0909f40284dfa743b4c50ef38
HShotaro9 <shotaro.10965@gmail.com>
2023-07-27 05:13:16 +0900

    initial commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftUI_Utility",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftUI_Utility",
+            targets: ["SwiftUI_Utility"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftUI_Utility"),
+        .testTarget(
+            name: "SwiftUI_UtilityTests",
+            dependencies: ["SwiftUI_Utility"]),
+    ]
+)
